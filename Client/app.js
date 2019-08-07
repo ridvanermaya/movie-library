633864ec12fa33e67ee3b32c304bc1038c40b24a
Ridvan Ermaya <ridvanermaya@gmail.com>
2019-08-07 09:26:35 -0500

    Added GetData(), GetCount(), and AddMovie() functions to my app.js.

--- Client/app.js
+++ Client/app.js
@@ -1,5 +1,82 @@
+const uri = "api/MovieLibrary";
+let movies = null;
 
+function GetData() {
+    $.ajax({
+        type: "GET",
+        url: uri,
+        cache: false,
+        success: function(data) {
+            const tBody = $("#movies");
 
+            $(tBody).empty();
+            GetCount(data.length);
+
+            $.each(data, function(key, item) {
+                const tr = $("<tr></tr>")
+                    .append($("<td></td>").text(item.Title))
+                    .append($("<td></td>").text(item.Genre))
+                    .append($("<td></td>").text(item.Director))
+                    .append(
+                        $("<td></td>").append(
+                            $("<button>Edit</button>").on("click", function() {
+                                EditItem(item.MovieId);
+                            })
+                        )
+                    )
+                    .append(
+                        $("<td></td>").append(
+                            $("<button>Delete</button>").on("click", function() {
+                                DeleteItem(item.MovieId);
+                            })
+                        )
+                    );
+                    
+                tr.appendTo(tBody);
+            });
+
+            movies = data;
+        }
+    });
+}
+
+function AddMovie() {
+    const movie = {
+        Title: $("#add-title").val(),
+        Genre: $("#add-genre").val(),
+        Director: $("#add-director").val()
+    };
+
+    $.ajax({
+        type: "POST",
+        accepts: "application/json",
+        url: uri,
+        contentType: "application/json",
+        data: JSON.stringify(movie),
+        error: function(jqXHR, textStatus, errorThrown) {
+            alert("Something unexpected happened!");
+        },
+        success: function(result) {
+            GetData();
+            $("#add-title").val(""),
+            $("#add-genre").val(""),
+            $("#add-director").val("");
+        }
+    });
+}
+
+function GetCount(data) {
+    const el = $("#counter");
+    let name = "movie";
+    if (data) {
+        if (data > 1) {
+            name = "movies";
+        }
+        el.text(data + " " + name);
+    } else {
+        el.text("No " + name);
+    }
+}
 // const uri = "api/MovieLibrary";
 // let movieLibrary = null;
 // function getCount(data) {

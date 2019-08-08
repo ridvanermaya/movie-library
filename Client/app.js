839e34c2d99f6449535fe9ff55e98f7b9ba0f605
Ridvan Ermaya <ridvanermaya@gmail.com>
2019-08-08 09:50:04 -0500

    Added EditMovie() function to my app.js and now I'm able to update selected movie.

--- Client/app.js
+++ Client/app.js
@@ -17,11 +17,11 @@ function GetData() {
             $.each(movies, function(index, movie) {
                 let tr = "<tr></tr>";
                 let td = "<td></td>";
-                let divModal = `<div class="modal fade" id="edit-movie-${index}" tabindex="-1" role="dialog" aria-labelledby="editMovie" aria-hidden="true">
+                let divModal = `<div class="modal fade" id="edit-movie-${index + 1}" tabindex="-1" role="dialog" aria-labelledby="editMovie" aria-hidden="true">
                                     <div class="modal-dialog" role="document">
                                         <div class="modal-content">
                                             <div class="modal-header">
-                                                <h5 class="modal-title" id="edit-movie-${index}">Edit Movie</h5>
+                                                <h5 class="modal-title" id="edit-movie-${index + 1}">Edit Movie</h5>
                                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                 <span aria-hidden="true">&times;</span>
                                                 </button>
@@ -30,21 +30,21 @@ function GetData() {
                                                 <form>
                                                     <div class="form-group">
                                                         <label for="movie-title" class="col-form-label">Movie Title</label>
-                                                        <input type="text" class="form-control" id="movie-title-${index}">
+                                                        <input type="text" class="form-control" id="edit-movie-title-${index + 1}">
                                                     </div>
                                                     <div class="form-group">
                                                         <label for="movie-genre" class="col-form-label">Movie Genre</label>
-                                                        <input type="text" class="form-control" id="movie-genre-${index}">
+                                                        <input type="text" class="form-control" id="edit-movie-genre-${index + 1}">
                                                     </div>
                                                     <div class="form-group">
                                                         <label for="movie-director" class="col-form-label">Movie Director</label>
-                                                        <input type="text" class="form-control" id="movie-director-${index}">
+                                                        <input type="text" class="form-control" id="edit-movie-director-${index + 1}">
                                                     </div>
                                                 </form>
                                             </div>
                                             <div class="modal-footer">
                                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                                <button type="button" class="btn btn-primary" id="edit_movie_${index}">Edit Movie</button>
+                                                <button onclick="EditMovie(${movie.movieId})" type="button" class="btn btn-primary" id="edit_movie_${index + 1}">Update Movie</button>
                                             </div>
                                         </div>
                                     </div>
@@ -58,56 +58,19 @@ function GetData() {
                     ).append(
                         $(td).text(movie.director)
                     ).append(
-                        `<button id="btn-edit-movie-${index}" type="button" class="btn btn-primary" data-toggle="modal" data-target="#edit-movie-${index}">Edit</button>`
+                        `<button id="btn-edit-movie-${index + 1}" type="button" class="btn btn-primary" data-toggle="modal" data-target="#edit-movie-${index + 1}">Edit</button>`
                     ).append(
-                        `<button id="btn-delete-movie-${index}"type="button" class="btn btn-primary">Delete</button>`
+                        `<button id="btn-delete-movie-${index + 1}"type="button" class="btn btn-primary">Delete</button>`
                     )
                 );
                 $(".modals").append($(divModal));
-                $(`#movie-title-${index}`).val(movie.title);
-                $(`#movie-genre-${index}`).val(movie.genre);
-                $(`#movie-director-${index}`).val(movie.director);
+                $(`#edit-movie-title-${index + 1}`).val(movie.title);
+                $(`#edit-movie-genre-${index + 1}`).val(movie.genre);
+                $(`#edit-movie-director-${index + 1}`).val(movie.director);
             });
         }
     });
 }
-
-// function GetData() {
-//     $.ajax({
-//         type: "GET",
-//         url: uri,
-//         success: function(data) {
-//             const tBody = $("#movies");
-
-//             $(tBody).empty();
-//             GetCount(data.length);
-
-//             $.each(data, function(i, item) {
-//                 let tr = "<tr>";
-//                     tr += "<td>" + item.title + "</td>"
-//                     + "<td>" + item.genre + "</td>"
-//                     + `<td> ${item.director} </td>`
-//                     + "<td>"
-//                     + `<button id="edit_button_${i}">Edit</button>`
-//                     + "</td>"
-//                     + "<td>"
-//                     + `<button id="delete_button_${i}">Delete</button>`
-//                     + "</td>"
-//                     + "</tr>";
-
-//                 tBody.append(tr);
-                
-//                 $(`#edit_button_${i}`).on("click", function() {
-//                     EditMovie(item.movieId);
-//                 })
-//                 $(`#delete_button_${i}`).on("click", function() {
-//                     DeleteMovie(item.movieId);
-//                 })
-//             movies = data;
-//             })
-//         }
-//     });
-// }
 
 function AddMovie() {
     const movie = {
@@ -147,117 +110,28 @@ function GetCount(data) {
     }
 }
 
+
 function EditMovie(movieId)
 {
-    $.each(movies, function(index, movie) {
-        if (movie.movieId === movieId)
-        {
-            $()
+    const movie = {
+        MovieId: movieId,
+        Title: $(`#edit-movie-title-${movieId}`).val(),
+        Genre: $(`#edit-movie-genre-${movieId}`).val(),
+        Director: $(`#edit-movie-director-${movieId}`).val()
+    };
+
+    $.ajax({
+        url: uri + "/" + movieId,
+        type: "PUT",
+        accepts: "application/json",
+        contentType: "application/json",
+        data: JSON.stringify(movie),
+        success: function(result) {
+            GetData();
         }
-    })
+    });
 }
 
-// function EditMovie(id) {
-//     $.each(movies, function(i, item) {
-//         if (item.MovieId === id) {
-//             $("#edit-movie-id").val(item.movieId);
-//             $("#edit-title").val(item.title);
-//             $("#edit-genre").val(item.genre);
-//             $("#edit-director").val(item.director);
-//         }
-//     });
-//     $("#spoiler").css({ display: "block"});
-// }
-
-
-// const uri = "api/MovieLibrary";
-// let movieLibrary = null;
-// function getCount(data) {
-//     const el = $('#counter');
-//     let name = "movie";
-//     if(data) {
-//         if(data > 1){
-//             name = "movies";
-//         }
-//         el.text(data + " " + name);
-//     } else {
-//         el.text("No " + name);
-//     }
-// }
-
-// $(document).ready(function() {
-//     getData();
-// });
-
-// function getData() {
-//     $.ajax({
-//         type: "GET",
-//         url: uri,
-//         cache: false,
-//         success: function(data) {
-//             const tBody = $('#movie-library');
-
-//             $(tBody).empty();
-
-//             getCount(data.Length);
-
-//             $.each(data, function(key, item) {
-//                 const tr = $("<tr></tr>")
-//                     .append(
-//                         $("<td></td>").append(
-//                             $("<input/>", {
-//                                 type: "text"
-//                             })
-//                         )
-//                     )
-//                     .append($("<td></td>").text(item.Title))
-//                     .append(
-//                         $("<td></td>").append(
-//                             $("<button>Edit</button>").on("click", function() {
-//                                 editItem(item.id);
-//                             })
-//                         )
-//                     )
-//                     .append(
-//                         $("<td></td>").append(
-//                             $("<button>Delete</button>").on("click", function() {
-//                                 deleteItem(item.id);
-//                             })
-//                         )
-//                     );
-
-//                     tr.appendTo(tBody);
-//             });
-
-//             movieLibrary = data;
-//         }
-//     });
-// }
-
-// function addItem() {
-//     const item = {
-//         Title: $("#add-title").val(),
-//         Genre: $("#add-genre").val(),
-//         Director: $("#add-director").val()
-//     };
-
-//     $.ajax({
-//         type: "POST",
-//         accepts: "application/json",
-//         url: uri,
-//         contentType: "application/json",
-//         data: JSON.stringify(item),
-//         error: function(jqXHR, textStatus, errorThrown) {
-//             alert("Something went wrong!");
-//         },
-//         success: function(result) {
-//             getData();
-//             $("#add-title").val(""),
-//             $("#add-genre").val(""),
-//             $("#add-director").val("");
-//         }
-//     });
-// }
 
 // function deleteItem(id) {
 //     $.ajax({
